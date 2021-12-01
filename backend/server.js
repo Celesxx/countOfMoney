da48b140ca16c8158e394d4a7d28c193c060f945
gabinrn <gabin.neron@epitech.eu>
2021-12-01 22:09:29 +0100

    feat: working dockerfile + dockercompose for api and database

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,11 @@ app.use(bodyParser.json({limit: "50mb"}));
     
 // Connecting to the database
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
 .then(async() => { console.log("Successfully connected to MongoDB."); })
 .catch(err => 
 {
-    console.log('Could not connect to MongoDB.')
+    console.log('Could not connect to MongoDB.' + process.env.MONGODB_URI)
     console.log(err)
     process.exit()
 });
@@ -30,8 +30,8 @@ require('./routeur/login.router.js')(app)
 
 var server = app.listen(process.env.PORT || 4000, function () 
 { 
-  var host = server.address().address
-  var port = server.address().port    
+  var host = "localhost" // server.address().address
+  var port = "4000" // server.address().port    
   console.log("App listening at http://%s:%s", host, port) 
 })
 
