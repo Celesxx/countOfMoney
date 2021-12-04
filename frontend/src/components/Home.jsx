71cfa07d848210b80c5b291ec5054eae0183ff4a
lucastoni <lucas.sola@epitech.eu>
2021-12-04 19:12:21 +0100

    feat:add navbar & router.js, delete useless files and clear frontend

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import '../styles/Home.css'
+
+const Home = () => {
+  return (
+    <section className="home">
+      <div className="main-comp">
+        AH OUI
+      </div>
+    </section>
+  )
+}
+
+export default Home
