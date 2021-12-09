7f10c5a7745530be26cf8de25aeff0f718a02b62
gabinrn <gabin.neron@epitech.eu>
2021-12-09 11:20:40 +0100

    Post and Get added

--- backend/server.js
+++ backend/server.js
@@ -20,19 +20,19 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 .then(async() => { console.log("Successfully connected to MongoDB."); })
 .catch(err => 
 {
-    console.log('Could not connect to MongoDB.' + process.env.MONGODB_URI)
+    console.log('Could not connect to MongoDB.')
     console.log(err)
     process.exit()
 });
 
 require('./routeur/user.router.js')(app)
 require('./routeur/login.router.js')(app)
+require('./routeur/crypto.router.js')(app)
 
 var server = app.listen(process.env.PORT || 4000, function () 
 { 
-  var host = "localhost" // server.address().address
-  var port = "4000" // server.address().port    
-  console.log("App listening at http://%s:%s", host, port) 
+  var port = server.address().port    
+  console.log("App listening at http://localhost:%s", port) 
 })
 
 module.exports = server;
